eab3f7b2f0ea64c5c5796de3d221b61d9e793e43
Blake Wood Jr <blakewoodjr84@gmail.com>
2023-01-19 12:37:54 -0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Timesheet',
+      theme: ThemeData.dark(),
+      home: const TimeSheet(),
+    );
+  }
+}
+
+class TimeSheet extends StatefulWidget {
+  const TimeSheet({super.key});
+
+  @override
+  State<TimeSheet> createState() => _TimeSheetState();
+}
+
+class _TimeSheetState extends State<TimeSheet> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
